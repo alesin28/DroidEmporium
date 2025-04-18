66a7c5529dcd37e8070d2685896d5ef802f51ec8
Alessandro Sinibaldi <alsn647@protonmail.com>
2025-04-18 19:43:19 +0200

    Implementado librerías de testing

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -35,6 +35,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.kotlin.test)
+
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
